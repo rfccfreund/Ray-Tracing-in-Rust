5bac49df8f51808b46512d3c41c9a75c40d3e30d
rfccfreund <rmfreund@gmail.com>
2022-05-28 22:03:51 -0500

    added normal

--- main.rs
+++ main.rs
@@ -24,6 +24,10 @@ fn main() {
     let origin = r3_vector::new(0.0, 0.0, 0.0);
     let horizontal = r3_vector::new(viewport_width, 0.0, 0.0);
     let vertical =r3_vector::new(0.0, viewport_height, 0.0);
+    // calculates the lower left corner of the screen by taking the center (origin)
+    // and shifting to the left by the horizontal vector (viewport width/2) and
+    // shifting down by the vertical vector (viewport_height/2) and into the screen
+    // by the focal length
     let lower_left_corner = &origin - 
         &horizontal/2.0 - &vertical/2.0 - r3_vector::new(0.0, 0.0, focal_length);
 
@@ -32,15 +36,17 @@ fn main() {
     
     println!("P3\n{} {}\n255", image_width, image_height);
 
+    //interates over the entire screen starting in the bottom left
     for i in (0..image_height as i32).rev() {
         for  j in 0..image_width {
             let start = r3_vector::new(0.0, 0.0, 0.0);
             let org = r3_vector::new(0.0, 0.0, 0.0);
+            //u and j go from 0 to 1  
             let u = j as f64 / (image_width-1) as f64;
             let v = i as f64 / (image_height-1.0);
             let r = Ray::new(org, &lower_left_corner + &horizontal * u + &vertical * v - &start);
             let pixel_color = ray_color(&r);
-            out_color(&pixel_color);
+            out_color(&pixel_color); 
         }
     }   
 }
--- ray_h.rs
+++ ray_h.rs
@@ -30,21 +30,28 @@ pub mod ray {
         }
     }
 
-    pub fn hit_sphere(center: &r3_vector, radius: f64, ray: &Ray ) -> bool {
+    pub fn hit_sphere(center: &r3_vector, radius: f64, ray: &Ray ) -> f64 {
         let oc: r3_vector = ray.origin() - center;
         let a: f64 = ray.direction().dot(ray.direction());
         let b: f64 = 2.0 * &oc.dot(ray.direction());
         let c: f64 = &oc.dot(&oc) - &radius * &radius;
         let discriminant = &b * &b - 4.0 * &a * &c;
-        discriminant > 0.0  
+        if (discriminant < 0.0) {
+            return -1.0;
+        }
+        else {
+            return (-&b - discriminant.sqrt()) / (2.0*&a);
+        }  
     }
     
     pub fn ray_color(r: &Ray) -> r3_vector {
-        if hit_sphere(&r3_vector::new(0.0,0.0,-1.0), 0.5, r) {
-            return r3_vector::new(1.0,0.0,0.0)
-            }
-        let unit_direction: r3_vector = unit_vector(&r.direction());
-        let t = 0.5*(&unit_direction.y()+1.0);
+        let t: f64 = hit_sphere(r3_vector::new(0.0,0.0,-1), 0.5, r);
+        if (t > 0.0) {
+            let N: r3_vector = (&r.location_at(&t) - r3_vector::new(0.0,0.0,-1.0)).unit_vector();
+            return 0.5*r3_vector::new(N.x()+1, N.y()+1, N.z()+1);
+        }
+        let unit_direction: r3_vector = r.direction().unit_vector();
+        let t = 0.5*(unit_direction.y() + 1.0);
         return r3_vector::new(1.0,1.0,1.0))*(1.0-&t) + r3_vector::new(0.5,0.7,1.0)*t
     }
 }
